9f2b6eaa463b3676ee7306a4eaef0c11d31d4649
flaviasergina <128700706+flaviasergina@users.noreply.github.com>
2023-06-21 23:23:45 -0300

    Lista 6 Completa
    Lista 6 Completa

---
+++ Program.cs
@@ -0,0 +1,76 @@
+// See https://aka.ms/new-console-template for more information
+using Atividade6StringArquivos;
+int resposta;
+Console.WriteLine("Escolha a questão");
+resposta = int.Parse(Console.ReadLine());
+
+switch (resposta)
+{
+    case 1:
+        questao1.rodar();
+        break;
+}
+
+switch (resposta)
+{
+    case 2:
+        questao2.rodar();
+        break;
+}
+
+switch (resposta)
+{
+    case 3:
+        questao3.rodar();
+        break;
+}
+
+
+switch (resposta)
+{
+    case 4:
+        questao4.rodar();
+        break;
+}
+
+switch (resposta)
+{
+    case 5:
+        questao5.rodar();
+        break;
+}
+
+switch (resposta)
+{
+    case 6:
+        questao6.rodar();
+        break;
+}
+
+switch (resposta)
+{
+    case 7:
+        questao7.rodar();
+        break;
+}
+
+switch (resposta)
+{
+    case 8:
+        questao8.rodar();
+        break;
+}
+
+switch (resposta)
+{
+    case 9:
+        questao8.rodar();
+        break;
+}
+
+switch (resposta)
+{
+    case 10:
+        questao8.rodar();
+        break;
+}
